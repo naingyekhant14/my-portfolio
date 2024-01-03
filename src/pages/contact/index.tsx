8cd2ceda07f5b7097ebc46241bd8d7ed0cb07e34
naingyekhant14 <naingyekhant14@gmail.com>
2024-01-03 17:43:03 +0630

    adding home page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,7 +1,17 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Grid container>
+      <Grid sx={{ bgcolor: "red" }} item xs={12} sm={12} md={6}>
+        h
+      </Grid>
+      <Grid sx={{ bgcolor: "green" }} item xs={12} sm={12} md={6}>
+        h
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,103 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Box, Button, CardMedia, Grid, Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const gridStyle = {
+  display: "flex",
+  justifyContent: "center",
+  height: "90vh",
+  alignItems: "center",
+};
 
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <Grid container>
+      <Grid sx={gridStyle} item xs={12} sm={12} md={6}>
+        <Box>
+          <Typography variant="h3" sx={{ pt: { xs: 5, sm: 0 } }}>
+            Web Developer
+          </Typography>
+          <Typography sx={{ color: "success.main", my: 3 }} variant="h1">
+            Naing Ye Khant
+          </Typography>
+          <Typography variant="body1" sx={{ color: "info.main", mb: 3 }}>
+            Hello, I'm naing ye khant, I'm a website developer . I can quickly
+            create websites using javascript framework with NextJs. I’m Aspiring
+            Junior Web Developer seeking an entry-level position.
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "success.main",
+              color: "primary.main",
+              textTransform: "capitalize",
+              mr: 5,
+              mb: { xs: 3, sm: 0 },
+              ":hover": {
+                backgroundColor: "secondary.main",
+                color: "success.main",
+              },
+            }}
+          >
+            <Typography variant="h6" sx={{ fontWeight: 800 }}>
+              Download CV
+            </Typography>
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "success.main",
+              color: "primary.main",
+              textTransform: "capitalize",
+              ":hover": {
+                backgroundColor: "secondary.main",
+                color: "success.main",
+              },
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: 800,
+              }}
+            >
+              Hireing Me
+            </Typography>
+          </Button>
+          <Typography
+            variant="body1"
+            sx={{ mt: 3, alignItems: "center", display: "flex" }}
+          >
+            Check out of my:
+            <Link href={"https://github.com/naingyekhant14"}>
+              <Button>
+                <GitHubIcon
+                  sx={{
+                    color: "success.main",
+                    fontSize: 38,
+                    ml: 2,
+                    cursor: "pointer",
+                    ":hover": {
+                      color: "info.main",
+                    },
+                  }}
+                />
+              </Button>
+            </Link>
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid sx={gridStyle} item xs={12} sm={12} md={6}>
+        <CardMedia
+          component="img"
+          image="./images/hero.png"
+          sx={{
+            maxWidth: 300,
+            borderRadius: "50%",
+            height: { xs: 270, sm: 320 },
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
 }
